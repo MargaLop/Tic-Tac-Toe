45ce3fdadc7c9afb8bf34f6214418006a0687906
Marga <margalppalacios@gmail.com>
2022-05-02 14:52:20 +0200

    crear clase casilla

--- casilla.py
+++ casilla.py
@@ -1,2 +1,9 @@
-class Casilla(self,jugada):
-    self.jugada = jugada
+class Casilla:
+    def __init__(self, row, column, jugada):
+        self.row = row
+        self.column = column
+        self.jugada = jugada
+
+    def guarda_ficha(self):
+        self.buffer =[]
+        self.buffer.append(self.jugada)
