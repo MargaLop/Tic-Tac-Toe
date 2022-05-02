fce1b19e157c88c8a096fc38cb65a947debc5b0c
Juampa <juan.pa.pe.co.98@gmail.com>
2022-05-02 13:49:18 +0200

    "Avances" en class tablero

--- tablero.py
+++ tablero.py
@@ -1,6 +1,27 @@
+from jinja2 import ChoiceLoader
+# from casilla import Casilla
+
+
 class Tablero:
-    def __init__(self,celdas,r,c):
-        self.celdas = celdas
-        self.r = r
-        self.c = c
-    
+    def __init__(self, rows, columns):
+        self.r = rows
+        self.c = columns
+        self.casilla = []
+        for row in range(self.r):
+            for col in range(self.c):
+                self.casilla.append(Casilla())
+    pass
+
+    def dibujaCasilla(self, altura, ancho):
+        altura = 5
+        ancho = 5
+        buffer_casilla = ""
+        celda = ancho * altura
+        print("""+-----+-----+-----+")
+        |     |     |     |")
+        +-----+-----+-----+")
+        |     |     |     |")
+        +-----+-----+-----+")
+        |     |     |     |")
+        +-----+-----+-----+""")
+
