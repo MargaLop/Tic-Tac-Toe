000c3fdefe98144a2256b5f39384c10dd4e81a81
Juampa <juan.pa.pe.co.98@gmail.com>
2022-05-03 11:45:12 +0200

    Ideas para class tablero (todo muy verde)

--- tablero.py
+++ tablero.py
@@ -1,4 +1,3 @@
-from jinja2 import ChoiceLoader
 # from casilla import Casilla
 
 
@@ -12,16 +11,39 @@ class Tablero:
                 self.casilla.append(Casilla())
     pass
 
-    def dibujaCasilla(self, altura, ancho):
-        altura = 5
-        ancho = 5
-        buffer_casilla = ""
-        celda = ancho * altura
-        print("""+-----+-----+-----+")
-        |     |     |     |")
-        +-----+-----+-----+")
-        |     |     |     |")
-        +-----+-----+-----+")
-        |     |     |     |")
-        +-----+-----+-----+""")
+    def make_output_buffer(input_buffer):
+        def make_line(position=0, size=80):
+            linechars = [
+                ['+', '+'],
+                ['+', '+'],
+                ['+', '+']
+            ]
+
+            linechar = linechars[position]
+
+            return ' ' + linechar[0] + '-' * (size + 2) + linechar[1]
+
+        def make_linetext(line, size=80):
+
+            return ' | ' + line.ljust(size) + ' | '
+
+        output_buffer = []
+
+        longest_line = max(input_buffer, key=len)
+        line_len = len(longest_line)
+
+        output_buffer.append(make_line(0, line_len))
+        first = True
+        for line in input_buffer:
+            output_buffer.append(make_linetext(line, line_len))
+            if first:
+                first = False
+                output_buffer.append(make_line(1, line_len))
+
+        output_buffer.append(make_line(2, line_len))
+
+        return output_buffer
+
+       
+
 
